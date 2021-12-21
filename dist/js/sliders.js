5d3a6e1066f8ebc05fddadb09de705ed74d5c793
Valeriy Yeroshenko <valeriy.yeroshenko@gmail.com>
2021-12-21 18:26:07 +0200

    update promo video, remove fb pixel

--- dist/js/sliders.js
+++ dist/js/sliders.js
@@ -1,82 +1,84 @@
 export function tariffSlider() {
+
     new Swiper('.tariffs__block', {
-        spaceBetween: 0,
+  
+        spaceBetween: 15,
         slidesPerView: 1,
         grabCursor: true,
         pagination: {
-            el: '.swiper-pagination',
-            clickable: true,
+          el: '.swiper-pagination',
+          clickable: true,
         },
-
+        
         navigation: {
-            nextEl: '.tariffs__block--arrow._next',
-            prevEl: '.tariffs__block--arrow._prev',
+          nextEl: '.tariffs__block--arrow._next',
+          prevEl: '.tariffs__block--arrow._prev',
         },
         breakpoints: {
-            1180: {
-                slidesPerView: 2,
-                spaceBetween: 72,
-                centeredSlides: false,
-
-            },
-            600: {
-                spaceBetween: 32,
-                slidesPerView: 2,
-                centeredSlides: false,
-            }
+          1180: {
+            slidesPerView: 2,
+            spaceBetween: 72,
+            centeredSlides: false,
+        
+          },
+          768: {
+            centeredSlides: true,
+            spaceBetween: 15,
+            slidesPerView: 3,
+        
+          }
         }
     });
 
 }
-
 let slider;
 export let reviewsSlider = {
-    desktopMode: function () {
-        if (slider) slider.destroy();
+    desktopMode: function() {
+        if(slider) slider.destroy();
         slider = new Swiper('.reviews__block', {
-
-            spaceBetween: 0,
-            slidesPerView: 1,
-            centeredSlides: true,
-            loop: true,
-            effect: "creative",
-            grabCursor: true,
-            creativeEffect: {
-                prev: {
-                    translate: ["-120%", 0, -500],
-                },
-                next: {
-                    translate: ["120%", 0, -500],
-                },
+    
+        spaceBetween: 0,
+        slidesPerView: 1,
+        centeredSlides: true,
+        loop: true,
+        effect: "creative",
+        grabCursor: true,
+        creativeEffect: {
+            prev: {      
+            translate: ["-120%", 0, -500],
             },
-
-
-            navigation: {
-                nextEl: '.reviews__item--slider-arrow._next',
-                prevEl: '.reviews__item--slider-arrow._prev',
+            next: {          
+            translate: ["120%", 0, -500],
             },
-            breakpoints: {
-                992: {
-                    slidesPerView: 3,
-                },
-            }
+        },
+        
+        
+        navigation: {
+            nextEl: '.reviews__item--slider-arrow._next',
+            prevEl: '.reviews__item--slider-arrow._prev',
+        },
+        breakpoints: {
+            992: {
+            slidesPerView: 3,
+            },
+        }
         });
     },
-    tableMode: function () {
-        if (slider) slider.destroy();
+    tableMode: function() {
+        if(slider) slider.destroy();
         slider = new Swiper('.reviews__block', {
-
-            spaceBetween: 0,
-            slidesPerView: 1,
-            centeredSlides: true,
-            loop: true,
-
-            navigation: {
-                nextEl: '.reviews__item--slider-arrow._next',
-                prevEl: '.reviews__item--slider-arrow._prev',
-            },
-
-
+    
+        spaceBetween: 0,
+        slidesPerView: 1,
+        centeredSlides: true,
+        loop: true,
+        
+        navigation: {
+            nextEl: '.reviews__item--slider-arrow._next',
+            prevEl: '.reviews__item--slider-arrow._prev',
+        },
+        
+        
         });
     },
 }
